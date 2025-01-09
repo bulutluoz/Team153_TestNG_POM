264ab1e5ceb877f90c9379e905fce086e98d9e95
bulutluoz <abbulutluoz@gmail.com>
2025-01-09 20:03:19 +0100

    Ders15 Sonu

--- src/test/java/tests/day15_testNG_FrameworkOlusturma/C03_DependsOnMethods.java
+++ src/test/java/tests/day15_testNG_FrameworkOlusturma/C03_DependsOnMethods.java
@@ -1,7 +1,11 @@
 package tests.day15_testNG_FrameworkOlusturma;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -32,25 +36,47 @@ public class C03_DependsOnMethods {
     @Test
     public void anasayfaTesti(){
         // 1- testotomasyonu anasayfaya gidip,
+        driver.get("https://www.testotomasyonu.com");
 
         // url'in testotomasyonu icerdigini test edin
+
+        String expectedUrlIcerik = "testotomasyonu";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedUrlIcerik));
 
     }
 
     @Test
     public void phoneAramaTesti(){
-        // 2- phone icin arama yapip,
+        // phone icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone" + Keys.ENTER);
 
-        // urun bulunabildigini test edin
+        // arama sonucunda urun bulunabildigini test edin
+        WebElement aramaSonucElementi = driver.findElement(By.xpath("//*[@*='product-count-text']"));
+
+        String unExpectedAramaSonucu = "0 Products Found";
+        String actualAramaSonucu = aramaSonucElementi.getText();
+
+        Assert.assertNotEquals(actualAramaSonucu,unExpectedAramaSonucu);
 
     }
 
     @Test
     public void urunIsimTesti(){
         // 3- ilk urunu tiklayip,
+        driver.findElement(By.xpath("(//*[@*='prod-img'])[1]"))
+                .click();
 
 
         // urun isminde case sensitive olmadan phone bulundugunu test edin
+        String expectedIsimIcerik = "phone";
+        String actualUrunIsmi = driver.findElement(By.xpath("//div[@class=' heading-sm mb-4']"))
+                                        .getText()
+                                        .toLowerCase();
+
+        Assert.assertTrue(actualUrunIsmi.contains(expectedIsimIcerik));
 
     }
 
