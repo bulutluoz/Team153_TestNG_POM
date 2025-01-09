0b9be0f9a242f6392346febf9db7582886f7a75e
bulutluoz <abbulutluoz@gmail.com>
2025-01-09 20:43:36 +0100

    Ders15 Sonu

---
+++ src/test/java/tests/day15_testNG_FrameworkOlusturma/C04_BasicDriverClassKullanimi.java
@@ -0,0 +1,24 @@
+package tests.day15_testNG_FrameworkOlusturma;
+
+public class C04_BasicDriverClassKullanimi {
+
+    public static void main(String[] args) {
+
+        /*
+            TestNG Page Object Model (POM) kullanir
+
+            POM temel olarak test icin ihtiyac duydugumuz
+            locate, driver ve test datalarinin dinamik olmasini
+            yani tek bir yerden yonetlebilmesini
+            ve Test method'larinin bu dinamik yontemle guncellenebilmesine dayanir
+
+            Ilk olarak driver objesini dinamik yapalim
+
+            POM TestBase Class kullanmak yerine
+            Driver class'inda
+            bize WebDriver objesi donduren bir getDriver()
+            ve webDriver'i kapatan quitDriver()
+            kullanmayi tercih etmistir
+         */
+    }
+}
