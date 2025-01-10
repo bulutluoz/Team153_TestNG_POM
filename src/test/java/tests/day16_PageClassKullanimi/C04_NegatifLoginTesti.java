854b30c47e5f231661b59c3b2b892ad0bc6cc26c
bulutluoz <abbulutluoz@gmail.com>
2025-01-10 20:35:08 +0100

    Ders16 Sonu

--- src/test/java/tests/day16_PageClassKullanimi/C04_NegatifLoginTesti.java
+++ src/test/java/tests/day16_PageClassKullanimi/C04_NegatifLoginTesti.java
@@ -1,6 +1,9 @@
 package tests.day16_PageClassKullanimi;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.Driver;
 
 public class C04_NegatifLoginTesti {
 
@@ -17,33 +20,69 @@ public class C04_NegatifLoginTesti {
     @Test
     public void gecersizPasswordTesti(){
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.testotomasyonu.com/");
         //2- account linkine basin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+
         //3- bir test method’u olusturun.
         //	- gecerli email, gecersiz password
+        testotomasyonuPage.loginSayfasiEmailKutusu.sendKeys("wise@gmail.com");
+        testotomasyonuPage.loginSayfasiPasswordKutusu.sendKeys("090909");
+
         //4- Login butonuna basarak login olmayi deneyin
+        testotomasyonuPage.loginSayfasiSubmitButonu.click();
+
         //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testotomasyonuPage.loginSayfasiEmailKutusu.isDisplayed());
+        Driver.quitDriver();
 
     }
 
     @Test
     public void gecersizEmailTesti(){
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.testotomasyonu.com/");
+
         //2- account linkine basin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+
         //3- bir test method’u olusturun.
         //	- gecersiz email, gecerli password
-        //4- Login butonuna basarak login olmayi deneyin
-        //5- Basarili olarak giris yapilamadigini test edin
+        testotomasyonuPage.loginSayfasiEmailKutusu.sendKeys("sanane@gmail.com");
+        testotomasyonuPage.loginSayfasiPasswordKutusu.sendKeys("12345");
 
+        //4- Login butonuna basarak login olmayi deneyin
+        testotomasyonuPage.loginSayfasiSubmitButonu.click();
+
+        //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testotomasyonuPage.loginSayfasiEmailKutusu.isDisplayed());
+
+        Driver.quitDriver();
     }
 
     @Test
     public void gecersizEmailGecersizPasswordTesti(){
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.testotomasyonu.com/");
+
         //2- account linkine basin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+
         //3- bir test method’u olusturun.
-        //	- gecersiz email, gecersiz password.
+        //	- gecersiz email, gecersiz password
+        testotomasyonuPage.loginSayfasiEmailKutusu.sendKeys("sanane@gmail.com");
+        testotomasyonuPage.loginSayfasiPasswordKutusu.sendKeys("090909");
+
         //4- Login butonuna basarak login olmayi deneyin
+        testotomasyonuPage.loginSayfasiSubmitButonu.click();
+
         //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testotomasyonuPage.loginSayfasiEmailKutusu.isDisplayed());
+
+        Driver.quitDriver();
 
     }
 }
