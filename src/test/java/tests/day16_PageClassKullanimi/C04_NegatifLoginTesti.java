f093b6c74e3ba88de00b2ac6ee4246ae730a8fa6
bulutluoz <abbulutluoz@gmail.com>
2025-01-10 20:13:14 +0100

    Ders16 Sonu

---
+++ src/test/java/tests/day16_PageClassKullanimi/C04_NegatifLoginTesti.java
@@ -0,0 +1,49 @@
+package tests.day16_PageClassKullanimi;
+
+import org.testng.annotations.Test;
+
+public class C04_NegatifLoginTesti {
+
+    //1- https://www.testotomasyonu.com/ anasayfasina gidin
+    //2- account linkine basin
+    //3-  3 farkli test method’u olusturun.
+    //	- gecerli email, gecersiz password
+    //	- gecersiz email, gecerli password
+    //	- gecersiz email, gecersiz password.
+    //4- Login butonuna basarak login olmayi deneyin
+    //5- Basarili olarak giris yapilamadigini test edin
+
+
+    @Test
+    public void gecersizPasswordTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- account linkine basin
+        //3- bir test method’u olusturun.
+        //	- gecerli email, gecersiz password
+        //4- Login butonuna basarak login olmayi deneyin
+        //5- Basarili olarak giris yapilamadigini test edin
+
+    }
+
+    @Test
+    public void gecersizEmailTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- account linkine basin
+        //3- bir test method’u olusturun.
+        //	- gecersiz email, gecerli password
+        //4- Login butonuna basarak login olmayi deneyin
+        //5- Basarili olarak giris yapilamadigini test edin
+
+    }
+
+    @Test
+    public void gecersizEmailGecersizPasswordTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- account linkine basin
+        //3- bir test method’u olusturun.
+        //	- gecersiz email, gecersiz password.
+        //4- Login butonuna basarak login olmayi deneyin
+        //5- Basarili olarak giris yapilamadigini test edin
+
+    }
+}
