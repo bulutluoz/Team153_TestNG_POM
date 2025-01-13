07dfe6ef769a0c9abc582b166e85fba51d5fbfbb
bulutluoz <abbulutluoz@gmail.com>
2025-01-13 20:08:30 +0100

    Ders17 Sonu

--- src/test/java/tests/day17_testDatalariniDinamikYapma/C02_DinamikNegatifLoginTesti.java
+++ src/test/java/tests/day17_testDatalariniDinamikYapma/C02_DinamikNegatifLoginTesti.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import pages.TestotomasyonuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class C02_DinamikNegatifLoginTesti {
 
@@ -12,18 +13,22 @@ public class C02_DinamikNegatifLoginTesti {
     public void gecersizPasswordTesti(){
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+
         //2- account linkine basin
         TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
         testotomasyonuPage.accountLinki.click();
+
         //3-  3 farkli test method’u olusturun.
         // - gecerli email, gecersiz password girin
         testotomasyonuPage.loginSayfasiEmailKutusu
                             .sendKeys(ConfigReader.getProperty("toGecerliEmail"));
         testotomasyonuPage.loginSayfasiPasswordKutusu
                             .sendKeys(ConfigReader.getProperty("toGecersizPassword"));
+
         //4- Login butonuna basarak login olmayi deneyin
         testotomasyonuPage.loginSayfasiSubmitButonu
                             .click();
+
         //5- Basarili olarak giris yapilamadigini test edin
         Assert.assertTrue(testotomasyonuPage.loginSayfasiEmailKutusu.isDisplayed());
         //6-sayfayi kapatin
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,7 +2,9 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -16,9 +18,19 @@ public class Driver {
 
         if (driver == null) {
 
-            driver = new ChromeDriver();
-
-
+            switch (kullanilacakBrowser){
+                case "safari" :
+                    driver = new SafariDriver();
+                    break;
+                case "firefox" :
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge" :
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
