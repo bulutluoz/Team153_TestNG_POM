e642f1b1b2c9e437a31861fb325c20b9f8539c46
bulutluoz <abbulutluoz@gmail.com>
2025-01-13 20:09:23 +0100

    Ders17 Sonu

---
+++ src/test/java/tests/day17_testDatalariniDinamikYapma/C03_AlisverisSepetiTesti.java
@@ -0,0 +1,17 @@
+package tests.day17_testDatalariniDinamikYapma;
+
+import org.testng.annotations.Test;
+
+public class C03_AlisverisSepetiTesti {
+
+    @Test
+    public void alisverisTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- belirlenmis arama kelimesi icin arama yapin
+        //3- Listelenen sonuclardan ilkini tiklayin
+        //4- urun ismini kaydedin ve urunu sepete ekleyin
+        //5- your cart linkine tiklayin
+        //6- kaydettiginiz urun ismi ile sepetteki urun isminin ayni oldugunu test edin
+        //7- sayfayi kapatin
+    }
+}
