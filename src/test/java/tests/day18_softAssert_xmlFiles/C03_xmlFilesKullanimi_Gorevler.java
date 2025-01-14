f1ac803457a8c44e615eb453bb4671611cfec08d
bulutluoz <abbulutluoz@gmail.com>
2025-01-14 20:48:08 +0100

    Ders18 Sonu

---
+++ src/test/java/tests/day18_softAssert_xmlFiles/C03_xmlFilesKullanimi_Gorevler.java
@@ -0,0 +1,20 @@
+package tests.day18_softAssert_xmlFiles;
+
+public class C03_xmlFilesKullanimi_Gorevler {
+
+    /*
+
+        Smoke Test icin
+
+        Day15 den C05 Class'ini
+        Day16 dan C01, C06 Class'larini
+        Day17 den C01, C02, C03 Class'larini
+        Day18 den C01, C02 Class'larini
+        toplu olarak calistirin
+
+
+        Mini Regression icin
+        day15, day16 ve day17 package'larini toplu olarak calistirin
+
+     */
+}
